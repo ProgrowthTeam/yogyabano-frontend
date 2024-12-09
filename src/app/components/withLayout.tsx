04e88dea07fdc7459a8021467216a79f68f819c8
Kishan <frontend@yogyabano.com>
2024-12-09 17:41:21 +0530

    fix build

--- src/app/components/withLayout.tsx
+++ src/app/components/withLayout.tsx
@@ -1,14 +1,7 @@
 import { useState, useEffect } from "react";
-import { usePathname, useRouter } from "next/navigation";
-import { JSX } from "react";
+import { usePathname } from "next/navigation";
 import Sidebar from "../components/Sidebar";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-  Popover,
-} from "@mui/material";
+import Navbar from "../components/Navbar"; // Import Navbar
 import {
   AccountCircle,
   ContentPaste,
@@ -16,9 +9,6 @@ import {
   GridView,
   Logout,
   Slideshow,
-  Notifications,
-  NotificationsNone,
-  ArrowBackIosNew,
 } from "@mui/icons-material";
 
 interface ActiveItem {
@@ -27,84 +17,6 @@ interface ActiveItem {
   path: string;
   subItems?: ActiveItem[];
 }
-
-interface NavbarProps {
-  activeItem: ActiveItem;
-}
-
-const Navbar: React.FC<NavbarProps> = ({ activeItem }) => {
-  const [notificationsOpen, setNotificationsOpen] = useState(false);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const router = useRouter();
-
-  const handleNotificationsClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-    setNotificationsOpen((prev) => !prev);
-  };
-
-  const handleClose = () => {
-    setNotificationsOpen(false);
-    setAnchorEl(null);
-  };
-
-  const handleBackClick = () => {
-    router.push("/create-course");
-  };
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (anchorEl && !anchorEl.contains(event.target as Node)) {
-        handleClose();
-      }
-    };
-
-    document.addEventListener("click", handleClickOutside);
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, [anchorEl]);
-
-  return (
-    <AppBar
-      position="static"
-      color="transparent"
-      elevation={0}
-      sx={{ borderBottom: "1px solid #B9C5CA" }}
-    >
-      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          {activeItem.path.startsWith("/create-course/") && (
-            <IconButton color="inherit" onClick={handleBackClick}>
-              <ArrowBackIosNew />
-            </IconButton>
-          )}
-          {activeItem.icon}
-          <Typography variant="h6" sx={{ flexGrow: 1, marginLeft: 2 }}>
-            {activeItem.name}
-          </Typography>
-        </div>
-        <IconButton color="inherit" onClick={handleNotificationsClick}>
-          {notificationsOpen ? <Notifications /> : <NotificationsNone />}
-        </IconButton>
-        <Popover
-          open={notificationsOpen}
-          anchorEl={anchorEl}
-          onClose={handleClose}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "right",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-        >
-          <Typography sx={{ p: 2 }}>Notifications</Typography>
-        </Popover>
-      </Toolbar>
-    </AppBar>
-  );
-};
 
 const defaultIcon: ActiveItem = {
   icon: <GridView />,
@@ -140,7 +52,7 @@ const bottomItems: ActiveItem[] = [
 ];
 
 const withLayout = (Component: React.ComponentType) => {
-  return (props: any) => {
+  const WrappedComponent = (props: any) => {
     const pathname = usePathname();
     const [activeItem, setActiveItem] = useState<ActiveItem>(defaultIcon);
 
@@ -184,6 +96,12 @@ const withLayout = (Component: React.ComponentType) => {
       </div>
     );
   };
+
+  WrappedComponent.displayName = `withLayout(${
+    Component.displayName || Component.name || "Component"
+  })`;
+
+  return WrappedComponent;
 };
 
 export default withLayout;
