e4ab2b2b672c67abbffb4d7c326f84f79cc4bd2b
Kishan <frontend@yogyabano.com>
2025-05-07 15:32:33 +0530

    update changes signup

--- src/app/services/signup.ts
+++ src/app/services/signup.ts
@@ -6,6 +6,7 @@ export async function signUpUser(email: string, password: string, options?: any)
     password,
     options: {
       data: {
+        isEmailVerified: true,
         ...options,
       }
     },
